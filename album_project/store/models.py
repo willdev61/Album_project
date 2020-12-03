7d55e744b5f84c588fed3a3f08fac58afe75c612
willdev <willdev61@gmail.com>
2020-12-03 08:56:29 +0100

    add models

--- album_project/store/models.py
+++ album_project/store/models.py
@@ -1,15 +1,22 @@
-# from django.db import models
-
-ARTISTS = {
-  'francis-cabrel': {'name': 'Francis Cabrel'},
-  'lej': {'name': 'Elijay'},
-  'rosana': {'name': 'Rosana'},
-  'maria-dolores-pradera': {'name': 'María Dolores Pradera'},
-}
+from django.db import models
 
 
-ALBUMS = [
-  {'name': 'Sarbacane', 'artists': [ARTISTS['francis-cabrel']]},
-  {'name': 'La Dalle', 'artists': [ARTISTS['lej']]},
-  {'name': 'Luna Nueva', 'artists': [ARTISTS['rosana'], ARTISTS['maria-dolores-pradera']]}
-]
+class Artist(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+class Contact(models.Model):
+    email = models.EmailField(max_length=100)
+    name = models.CharField(max_length=200)
+
+
+class Album(models.Model):
+    reference = models.IntegerField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    available = models.BooleanField(default=True)
+    title = models.CharField(max_length=200)
+    picture = models.URLField()
+
+
+class Booking(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    contacted = models.BooleanField(default=False)
