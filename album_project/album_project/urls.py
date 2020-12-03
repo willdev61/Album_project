d2370feb1347c6a1a21beae42a8663bc5f1e6642
willdev <willdev61@gmail.com>
2020-12-03 08:42:04 +0100

    create home page and listing albums

--- album_project/album_project/urls.py
+++ album_project/album_project/urls.py
@@ -1,12 +1,13 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from store import views
 
 urlpatterns = [
+    url(r'^$', views.index),
     url(r'^store/', include('store.urls')),
     url(r'^admin/', admin.site.urls)
 ]
-
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
