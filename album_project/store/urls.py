2f1e9b1f7c67e4b52ba4cd59dc660ea12872b9bd
willdev <willdev61@gmail.com>
2020-12-03 11:36:05 +0100

    add missing templates

--- album_project/store/urls.py
+++ album_project/store/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import url
 
 from . import views # import views so we can use them in urls.
 
+app_name = 'store'
 
 urlpatterns = [
-    url(r'^$', views.listing),
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail),
-    url(r'^search/$', views.search),
+    url(r'^$', views.listing, name='listing'),
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^search/$', views.search, name='search'),
 ]
