37f42c03e6ecd592182ed9ce948b1ca793074cad
willdev <willdev61@gmail.com>
2020-12-03 08:51:57 +0100

    pass arguments to a view

--- album_project/store/urls.py
+++ album_project/store/urls.py
@@ -5,4 +5,6 @@ from . import views # import views so we can use them in urls.
 
 urlpatterns = [
     url(r'^$', views.listing),
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail),
+    url(r'^search/$', views.search),
 ]
--- album_project/store/views.py
+++ album_project/store/views.py
@@ -10,3 +10,35 @@ def listing(request):
     albums = ["<li>{}</li>".format(album['name']) for album in ALBUMS]
     message = """<ul>{}</ul>""".format("\n".join(albums))
     return HttpResponse(message)
+
+
+def detail(request, album_id):
+    id = int(album_id) # make sure we have an integer.
+    album = ALBUMS[id] # get the album with its id.
+    artists = " ".join([artist['name'] for artist in album['artists']]) # grab artists name and create a string out of it.
+    message = "Le nom de l'album est {}. Il a été écrit par {}".format(album['name'], artists)
+    return HttpResponse(message)
+
+
+def search(request):
+    query = request.GET.get('query')
+    if not query:
+        message = "Aucun artiste n'est demandé"
+    else:
+        albums = [
+            album for album in ALBUMS
+            if query in " ".join(artist['name'] for artist in album['artists'])
+        ]
+
+        if len(albums) == 0:
+            message = "Misère de misère, nous n'avons trouvé aucun résultat !"
+        else:
+            albums = ["<li>{}</li>".format(album['name']) for album in albums]
+            message = """
+                Nous avons trouvé les albums correspondant à votre requête ! Les voici :
+                <ul>
+                    {}
+                </ul>
+            """.format("</li><li>".join(albums))
+
+    return HttpResponse(message)
