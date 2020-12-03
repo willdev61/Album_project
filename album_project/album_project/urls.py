a7a88376239b8378cba58ec2a028ef1b11fa9513
willdev <willdev61@gmail.com>
2020-12-03 08:31:58 +0100

    create a template folder and url file in store

--- album_project/album_project/urls.py
+++ album_project/album_project/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^store/', include('store.urls')),
+    url(r'^admin/', admin.site.urls)
 ]
 
 if settings.DEBUG:
