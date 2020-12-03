a3cf077f2e8e8390605d01943da91405cfd4874e
willdev <willdev61@gmail.com>
2020-12-03 11:21:06 +0100

    modifyng templates

--- album_project/store/views.py
+++ album_project/store/views.py
@@ -13,7 +13,10 @@ def index(request):
     formatted_albums = ["<li>{}</li>".format(album.title) for album in albums]
     message = """<ul>{}</ul>""".format("\n".join(formatted_albums))
     template = loader.get_template('store/index.html')
-    return HttpResponse(template.render(request=request))
+    context = {
+        'albums': albums
+    }
+    return HttpResponse(template.render(context, request=request))
 
 
 def listing(request):
